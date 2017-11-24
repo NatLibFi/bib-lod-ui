55e9a4a35ef3d724a395fd2c984c94bf5734e8f1
Osma Suominen <osma.suominen@helsinki.fi>
2017-11-24 09:16:06 +0200

    format-specific views also for persons and concepts (with plenty of code duplication)

--- biblodui/views.py
+++ biblodui/views.py
@@ -1,8 +1,17 @@
 from flask import render_template, abort, redirect, request, make_response
 from flask_rdf import wants_rdf
 from flask_rdf.flask import returns_rdf
+from werkzeug.routing import BaseConverter
 
 from biblodui import app, model
+
+
+class RegexConverter (BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
 
 
 @app.route('/')
@@ -61,7 +70,7 @@ def bib_instance(instanceid):
     work = inst.get_work()
     return redirect("%s#I%s" % (work.url(), instanceid))
 
-@app.route('/au/pn/<personid>')
+@app.route('/au/pn/<regex("[0-9]+"):personid>')
 @returns_rdf
 def person_resource(personid):
     res = model.get_resource('http://urn.fi/URN:NBN:fi:au:pn:%s' % personid)
@@ -74,7 +83,32 @@ def person_resource(personid):
         response.headers['Vary'] = 'Accept'
         return response
 
-@app.route('/yso/<conceptid>')
+@app.route('/au/pn/<regex("[0-9]+"):personid>.<format>')
+def person_resource_format(personid, format):
+    res = model.get_resource('http://urn.fi/URN:NBN:fi:au:pn:%s' % personid)
+    if not res.exists():
+        abort(404)
+    if format == 'rdf':
+        response = make_response(res.graph.serialize(format='xml'))
+        response.headers['Content-Type'] = 'application/rdf+xml'
+    elif format == 'ttl':
+        response = make_response(res.graph.serialize(format='turtle'))
+        response.headers['Content-Type'] = 'text/turtle'
+    elif format == 'nt':
+        response = make_response(res.graph.serialize(format='nt'))
+        response.headers['Content-Type'] = 'application/n-triples'
+    elif format == 'json':
+        context = {"@vocab": "http://schema.org/"}
+        response = make_response(res.graph.serialize(format='json-ld', context=context))
+        response.headers['Content-Type'] = 'application/json'
+    elif format == 'html':
+        response = make_response(render_template('resource.html', title=res.name(), res=res))
+    else:
+        abort(404)
+    return response
+
+
+@app.route('/yso/<regex("p[0-9]+"):conceptid>')
 @returns_rdf
 def concept_resource(conceptid):
     res = model.get_resource('http://www.yso.fi/onto/yso/%s' % conceptid)
@@ -87,3 +121,26 @@ def concept_resource(conceptid):
         response.headers['Vary'] = 'Accept'
         return response
 
+@app.route('/yso/<regex("p[0-9]+"):conceptid>.<format>')
+def concept_resource_format(conceptid, format):
+    res = model.get_resource('http://www.yso.fi/onto/yso/%s' % conceptid)
+    if not res.exists():
+        abort(404)
+    if format == 'rdf':
+        response = make_response(res.graph.serialize(format='xml'))
+        response.headers['Content-Type'] = 'application/rdf+xml'
+    elif format == 'ttl':
+        response = make_response(res.graph.serialize(format='turtle'))
+        response.headers['Content-Type'] = 'text/turtle'
+    elif format == 'nt':
+        response = make_response(res.graph.serialize(format='nt'))
+        response.headers['Content-Type'] = 'application/n-triples'
+    elif format == 'json':
+        context = {"@vocab": "http://schema.org/"}
+        response = make_response(res.graph.serialize(format='json-ld', context=context))
+        response.headers['Content-Type'] = 'application/json'
+    elif format == 'html':
+        response = make_response(render_template('resource.html', title=res.name(), res=res))
+    else:
+        abort(404)
+    return response
